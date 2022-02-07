1024748e5b83b5ef143ace16b4a1d03c56c0fde5
ahmadilham22 <angilham2@gmail.com>
2022-02-08 00:31:47 +0700

    menambahkan illuminate Auth

--- app/Http/Controllers/AssetClientController.php
+++ app/Http/Controllers/AssetClientController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\AssetClient;
 use Illuminate\Http\Request;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 
 class AssetClientController extends Controller
 {
